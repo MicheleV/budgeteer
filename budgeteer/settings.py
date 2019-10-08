17a7ef708201be88d9daa8e48a83fa4cde4479ec
Michele Valsecchi <MicheleV@users.noreply.github.com>
2019-10-08 23:15:15 +0900

    Use .env file

--- budgeteer/settings.py
+++ budgeteer/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -18,14 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n(9l+=@dq2n&a!m04o17ov(c3k7^f(coy%*eel@=+faa!z)6@8'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+if 'y' in os.getenv("DJANGO_DEBUG_MODE"):
+    DEBUG = True
+else:
+    DEBUG = False
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+ALLOWED_HOSTS = [os.getenv("SITENAME")]
 
 
 # Application definition

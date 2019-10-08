5545e3f6823302ed98d24737595be3c93985d7d0
Michele Valsecchi <MicheleV@users.noreply.github.com>
2019-10-08 22:04:58 +0900

    Fix provisioning script

--- tools/update_sqlite3.sh
+++ tools/update_sqlite3.sh
@@ -3,8 +3,8 @@
 # GNU General Public License v3.0+
 # (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 # Sqlite3 update steps credits http://www.tohoho-web.com/django/tutorial.html
-need_to_install=$(sqlite3 --version | cut -d. -f2)
-if $[ need_to_install == 29]; then
+current_version=$(sqlite3 --version | cut -d. -f2)
+if [ $current_version == 7 ]; then
   cd /home/ec2-user/
   wget https://www.sqlite.org/2019/sqlite-autoconf-3290000.tar.gz
   tar zxvf ./sqlite-autoconf-3290000.tar.gz

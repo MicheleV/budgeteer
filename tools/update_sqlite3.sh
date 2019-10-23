6073c7a539faa1fac11d325dc3135c75fdc70a60
Michele Valsecchi <MicheleV@users.noreply.github.com>
2019-10-24 00:03:05 +0900

    Improve provisioning script.

--- tools/update_sqlite3.sh
+++ tools/update_sqlite3.sh
@@ -2,11 +2,12 @@
 # Copyright: (c) 2019, Michele Valsecchi <https://github.com/MicheleV>
 # GNU General Public License v3.0+
 # (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-# Sqlite3 update steps credits http://www.tohoho-web.com/django/tutorial.html
+
 current_version=$(sqlite3 --version | cut -d. -f1,2)
   if [ $? == 1 ] ||  [ "$current_version" != "3.29" ]; then
   echo "Install sqlite3"
   cd /home/$@/
+  # Steps credits http://www.tohoho-web.com/django/tutorial.html
   wget https://www.sqlite.org/2019/sqlite-autoconf-3290000.tar.gz
   tar zxvf ./sqlite-autoconf-3290000.tar.gz
   cd ./sqlite-autoconf-3290000

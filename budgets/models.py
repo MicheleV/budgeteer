cd6cfcbc759adcb28a8b04841d24d72aeee930fb
Michele Valsecchi <MicheleV@users.noreply.github.com>
2020-01-10 23:02:38 +0900

    Add is_archived field to Category model

--- budgets/models.py
+++ budgets/models.py
@@ -6,6 +6,7 @@ from django.db import models
 
 class Category(models.Model):
     text = models.CharField(max_length=20, default=None, unique=True)
+    is_archived = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.text}"

375b5596428b64e11c475933ee5ee06d8a686a8f
Michele Valsecchi <MicheleV@users.noreply.github.com>
2020-02-29 12:59:32 +0900

    Add Kickstarter file and shell script to start CentOS guest

--- tools/vm_creation/boot.sh
+++ tools/vm_creation/boot.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
+#
+# Start a Centos VM and reboot it
 
 # Get CentOS Minimal from http://isoredirect.centos.org/centos/7/isos/x86_64/
-# The vm is created with user "cloud-user", password "123456"
-sudo virt-install --name  \
---location ./CentOS-7-x86_64-Minimal.iso \
---initrd-inject ks.cfg \
---extra-args "ks=file:/ks.cfg console=ttyS0,115200" \
---memory=1024 --vcpus=1 --disk size=5 \
---noautoconsole --graphics vnc --serial pty --console pty
+# /var/lib/libvirt/images/CentOS-7-x86_64-Minimal-1908.iso should be owned by qemu:qemu
 
-# TODO: try to add --nographic instead of vnc
+# root user, password "root" user "cloud-user", password "123456" as per kickstart.cfg
+vm_name=my-new-vm
+iso_path=/var/lib/libvirt/images/CentOS-7-x86_64-Minimal.iso
+network_name=default
+
+# NOTE --wait=-1 make sure the reboot is issued after installation is complete
+# as per https://access.redhat.com/solutions/41976
+
+virt-install --name $vm_name \
+--location $iso_path \
+--initrd-inject kickstart.cfg \
+--memory=1024 --vcpus=1 --disk size=10 \
+--extra-args "ks=file:/kickstart.cfg console=ttyS0,115200" \
+--noautoconsole --wait=-1 --serial pty --console pty
+
+
+vm_ip=$(virsh net-dhcp-leases $network_name | grep $(virsh dumpxml $vm_name | grep "mac address" | awk -F\' '{ print $2}' ) | awk '{print $5}')
+echo "VM $vm_name IP is: $vm_ip"

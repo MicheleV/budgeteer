a2a5fd5293cdf8c3f01ec58144e2512d78d64903
Michele Valsecchi <MicheleV@users.noreply.github.com>
2020-03-15 13:08:05 +0900

    WIP

--- tools/update_sqlite3.sh
+++ tools/update_sqlite3.sh
@@ -4,28 +4,32 @@
 # (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 current_version=$(sqlite3 --version | cut -d. -f1,2)
-  if [ $? == 1 ] ||  [ "$current_version" != "3.29" ]; then
+if [ $? == 1 ] ||  [ "$current_version" != "3.29" ]; then
   echo "Install sqlite3"
-  cd /home/$@/
+  cd ~
   # Steps credits http://www.tohoho-web.com/django/tutorial.html
-  wget https://www.sqlite.org/2019/sqlite-autoconf-3290000.tar.gz
+  if [ ! -f ./sqlite-autoconf-3290000.tar.gz ]; then
+    wget https://www.sqlite.org/2019/sqlite-autoconf-3290000.tar.gz
+  fi
+
   tar zxvf ./sqlite-autoconf-3290000.tar.gz
   cd ./sqlite-autoconf-3290000
-  ./configure --prefix=/home/$@/opt/sqlite
+  ./configure
   make
   make install
   cd ..
-  rm -rf ./sqlite-autoconf-3290000 ./sqlite-autoconf-3290000.tar.gz
+  # rm -rf ./sqlite-autoconf-3290000 ./sqlite-autoconf-3290000.tar.gz
   sudo mv /usr/bin/sqlite3 /usr/bin/sqlite3_old
-  echo "export PATH='$PATH:/home/$@/opt/sqlite/bin'" >> /home/$@/.bashrc
-  echo "export LD_LIBRARY_PATH=/home/$@/opt/sqlite/lib" >> /home/$@/.bashrc
-  echo "export LD_RUN_PATH=/home/$@/opt/sqlite/lib" >> /home/$@/.bashrc
+  sudo mv mv /home/cloud-user/sqlite-autoconf-3290000/sqlite3 /usr/bin/
+  # echo "export PATH='$PATH:/home/$@/opt/sqlite/bin'" >> /home/$@/.bashrc
+  # echo "export LD_LIBRARY_PATH=/home/$@/opt/sqlite/lib" >> /home/$@/.bashrc
+  # echo "export LD_RUN_PATH=/home/$@/opt/sqlite/lib" >> /home/$@/.bashrc
 
-  # Set env varitables for gunicorn
-  echo "LD_LIBRARY_PATH=/home/$@/opt/sqlite/lib" >> /srv/$@/budgeteer/.env
-  echo "LD_RUN_PATH=/home/$@/opt/sqlite/lib" >> /srv/$@/budgeteer/.env
+  # # Set env varitables for gunicorn
+  # echo "LD_LIBRARY_PATH=/home/$@/opt/sqlite/lib" >> /srv/$@/budgeteer/.env
+  # echo "LD_RUN_PATH=/home/$@/opt/sqlite/lib" >> /srv/$@/budgeteer/.env
 
-  source /home/$@/.bashrc
+  # source /home/$@/.bashrc
   new_version=$(sqlite3 --version | cut -d. -f1,2)
   if [ $? == 1] ||  [ "$new_version" != "3.29" ]; then
     echo $new_version

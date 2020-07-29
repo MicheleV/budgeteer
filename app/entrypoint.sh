24030d1e2161d1d0ec9306f14fa80bbe2e06cd4c
Michele Valsecchi <MicheleV@users.noreply.github.com>
2020-07-29 18:34:15 +0900

    Makes web container wait for the db

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
+# Credits https://testdriven.io/blog/dockerizing-django-with-postgres-gunicorn-and-nginx/
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
 
-python /app/manage.py flush --no-input
-python /app/manage.py migrate
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+# This will tear down the database, comment out when needed :)
+# python manage.py flush --no-input
+
+python manage.py migrate
 
 exec "$@"

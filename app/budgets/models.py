f9addfe7b91ab1e4332e052e66febfe346519f66
Michele Valsecchi <MicheleV@users.noreply.github.com>
2020-08-14 22:22:04 +0900

    HACK to update current data. Migrations to be squashed later

--- app/budgets/models.py
+++ app/budgets/models.py
@@ -1,6 +1,7 @@
 # Copyright: (c) 2019, Michele Valsecchi <https://github.com/MicheleV>
 # GNU General Public License v3.0+
 # (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 
@@ -11,14 +12,27 @@ from django.urls import reverse
 
 
 class Category(models.Model):
+    # TODO: unique should be (name + created_by), as we now have
+    # multiple users now
     text = models.CharField(max_length=40, default=None, unique=True)
     is_archived = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, default=None,
+                                   null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f"{self.text}"
 
 
 class Expense(models.Model):
+    category = models.ForeignKey(Category, default=None, null=True,
+                                 on_delete=models.SET_NULL)
+    amount = models.IntegerField()
+    # Warning: keep in mind this will allow both empty strings AND NULL
+    # https://docs.djangoproject.com/en/2.2/ref/models/fields/#null
+    note = models.CharField(null=True, blank=True, max_length=150, default='')
+    date = models.DateField()
+    created_by = models.ForeignKey(User, default=None,
+                                   null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         id = self.category.id
@@ -27,33 +41,42 @@ class Expense(models.Model):
         date = self.date
         return f"{id}: ({note}), {amount}, {date}"
 
-    category = models.ForeignKey(Category, default=None, null=True,
-                                 on_delete=models.SET_NULL)
-    amount = models.IntegerField()
-    # Warning: keep in mind this will allow both empty strings AND NULL
-    # https://docs.djangoproject.com/en/2.2/ref/models/fields/#null
-    note = models.CharField(null=True, blank=True, max_length=150, default='')
-    date = models.DateField()
-
 
 class MonthlyBudget(models.Model):
+    # TODO: unique should be (category + created_by), as we now have
+    # multiple users now
     category = models.ForeignKey(Category, default=None, null=True,
                                  on_delete=models.SET_NULL)
     amount = models.IntegerField()
     date = models.DateField()
+    created_by = models.ForeignKey(User, default=None,
+                                   null=True, on_delete=models.SET_NULL)
 
     class Meta:
         unique_together = ('category', 'date')
 
 
 class IncomeCategory(models.Model):
+    # TODO: unique should be (text + created_by) as now we have
+    # multiple users now
     text = models.CharField(max_length=40, default=None, unique=True)
+    created_by = models.ForeignKey(User, default=None,
+                                   null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f"{self.text}"
 
 
 class Income(models.Model):
+    category = models.ForeignKey(IncomeCategory, default=None, null=True,
+                                 on_delete=models.SET_NULL)
+    amount = models.IntegerField()
+    # Warning: keep in mind this will allow both empty strings AND NULL
+    # https://docs.djangoproject.com/en/2.2/ref/models/fields/#null
+    note = models.CharField(null=True, blank=True, max_length=150, default='')
+    date = models.DateField()
+    created_by = models.ForeignKey(User, default=None,
+                                   null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         id = self.category.id
@@ -62,18 +85,14 @@ class Income(models.Model):
         date = self.date
         return f"{id}: {amount}, {note}, {date}"
 
-    category = models.ForeignKey(IncomeCategory, default=None, null=True,
-                                 on_delete=models.SET_NULL)
-    amount = models.IntegerField()
-    # Warning: keep in mind this will allow both empty strings AND NULL
-    # https://docs.djangoproject.com/en/2.2/ref/models/fields/#null
-    note = models.CharField(null=True, blank=True, max_length=150, default='')
-    date = models.DateField()
-
 
 class MonthlyBalanceCategory(models.Model):
+    # TODO: unique should be (text + created_by) not only text, as we have
+    # multiple users now
     text = models.CharField(max_length=40, default=None, unique=True)
     is_foreign_currency = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, default=None,
+                                   null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f"{self.text}"
@@ -84,16 +103,18 @@ class MonthlyBalanceCategory(models.Model):
 
 
 class MonthlyBalance(models.Model):
+    category = models.ForeignKey(MonthlyBalanceCategory, default=None,
+                                 null=True, on_delete=models.SET_NULL)
+    amount = models.IntegerField()
+    date = models.DateField()
+    created_by = models.ForeignKey(User, default=None,
+                                   null=True, on_delete=models.SET_NULL)
+
     def __str__(self):
         id = self.category.id
         amount = self.amount
         date = self.date
         return f"{id}: {amount}, {date}"
-
-    category = models.ForeignKey(MonthlyBalanceCategory, default=None,
-                                 null=True, on_delete=models.SET_NULL)
-    amount = models.IntegerField()
-    date = models.DateField()
 
     class Meta:
         unique_together = ('category', 'date')
@@ -103,13 +124,18 @@ class MonthlyBalance(models.Model):
 
 
 class Goal(models.Model):
+    amount = models.IntegerField()
+    # TODO: unique should be (name + created_by) not only name, as we have
+    # multiple users now
+    text = models.CharField(max_length=20, default=None, unique=True)
+    # FIXME: should note be unique... ?
+    note = models.CharField(max_length=50, default=None, unique=True)
+    is_archived = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, default=None,
+                                   null=True, on_delete=models.SET_NULL)
+
     def __str__(self):
         id = self.id
         amount = self.amount
         text = self.text
         return f"{id}: {amount}, {text}"
-
-    amount = models.IntegerField()
-    text = models.CharField(max_length=20, default=None, unique=True)
-    note = models.CharField(max_length=50, default=None, unique=True)
-    is_archived = models.BooleanField(default=False)

705a6bc9a30bd30fe5e516f24b0ce1f76ff78b93
Michele Valsecchi <MicheleV@users.noreply.github.com>
2020-10-07 21:54:53 +0900

    Add some js functions to speed up data insertion

--- app/budgets/static/js/main.js
+++ app/budgets/static/js/main.js
@@ -2,6 +2,18 @@
 function setInput(element, value){
   const formNum = element.id.replace("copy-button-","");
   const targetId = `id_form-${formNum}-amount`
-  print(targetId, value)
   document.getElementById(targetId).value = value
+}
+
+
+function setToday(element, value){
+  const today = new Date().toJSON().slice(0,10)
+  const targetId = `id_date`
+  document.getElementById(targetId).value = today
+}
+
+function firstDayOfThisMonth(element, value){
+  const today = new Date().toJSON().slice(0,8).concat('01')
+  const targetId = `id_date`
+  document.getElementById(targetId).value = today
 }
